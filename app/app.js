4c149c168f7ade13077cb8e75ff80e8e3acb1fd1
Suhas <suhas@example.com>
2023-11-01 14:39:13 +0530

    Updated workflow and app

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 exports.handler = async(event)=>{
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Lambda! This is v2 of the app'),
     };
     return response;
 };
