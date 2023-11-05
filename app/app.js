2089900f74c41c802f75008bb232c4c2f51b3b68
Suhas <suhas@example.com>
2023-11-05 18:14:26 +0530

    New Changes to Lambda

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 exports.handler = async(event)=>{
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! This is v2 of the app'),
+        body: JSON.stringify('Hello from Lambda! This is v3 of the app'),
     };
     return response;
 };
